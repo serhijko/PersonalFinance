1d65bc2060e27140914fc912c5d7b420362084cd
serhijko <sp_by@tut.by>
2022-05-11 13:23:00 -0300

    Create a class with text constants

--- src/personalfinance/PersonalFinance.java
+++ src/personalfinance/PersonalFinance.java
@@ -5,6 +5,16 @@
  */
 package personalfinance;
 
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import personalfinance.settings.Text;
+
 /**
  *
  * @author Serhij
@@ -15,8 +25,19 @@ public class PersonalFinance {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello");
+        init();
+        System.out.println(Text.get("PROGRAM_NAME"));
+        System.out.println(Arrays.toString(Text.getMonths()));
+    }
+    
+    private static void init() {
+        try {
+            Text.init();
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Roboto-Light.ttf")));
+        } catch (FontFormatException | IOException ex) {
+            Logger.getLogger(PersonalFinance.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
